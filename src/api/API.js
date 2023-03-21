118e4116fd4805d48d7c6ef41fb87d75e396b0f8
JefersonHernandez <52810595+JefersonHernandez@users.noreply.github.com>
2023-03-20 19:17:39 -0500

    otro comit

--- src/api/API.js
+++ src/api/API.js
@@ -1,11 +1,9 @@
-import axios from 'axios'
-import React from 'react'
+import axios from "axios";
+import React from "react";
 
-//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
-export const githubApi=axios.create({
-    baseURL:"https://api.github.com/repos/facebook/react",
-    headers:{
-        Authorization:"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"
-
-    }
-})
+export const githubApi = axios.create({
+  baseURL: "https://api.github.com/repos/facebook/react",
+  headers: {
+    Authorization: import.meta.env.VITE_GITHUBKEY,
+  },
+});
